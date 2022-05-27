afd4f2bb2c776c13575c2d589a9899c4d99432ba
Bellotta-P <bellotta.pietro@iisgalvanimi.edu.it>
2022-05-27 09:27:56 +0000

    servizio Clima

--- sitoweb/src/app/app.component.ts
+++ sitoweb/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  msg:string | undefined; 
   title = 'sitoweb';
+  
+  
+  clickEventMite(){
+    this.msg='Mite';
+    return this.msg;
+  }
 }
