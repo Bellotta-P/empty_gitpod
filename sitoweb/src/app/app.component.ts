db9e236f2983b5488ae570b225bd52fd008dfbc7
Bellotta-P <bellotta.pietro@iisgalvanimi.edu.it>
2022-05-27 12:00:50 +0000

    collegamento bottoni

--- sitoweb/src/app/app.component.ts
+++ sitoweb/src/app/app.component.ts
@@ -24,6 +24,43 @@ export class AppComponent {
     console.log(this.resp)
   }
 
+  clickEventFreddo(){
+    this.msg='Freddo';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  clickEventCaldo(){
+    this.msg='Caldo';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  clickEventNome_Regione_1(){
+    this.msg='Regione_1';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  clickEventNome_Regione_2(){
+    this.msg='Regione_2';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  clickEventNome_Regione_3(){
+    this.msg='Regione_3';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  clickEventNome_Regione_4(){
+    this.msg='Regione_4';
+    return this.msg;
+    console.log(this.resp)
+  }
+
+  
   getData(): void {
 		this.ClimaService.getData().subscribe(resp => this.resp = resp);
 	}
