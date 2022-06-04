9dd348675c29f503bee7ef1031622a29180090d1
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-06-04 18:04:13 +0000

    Progetto qasissimo finito

--- sitoweb/src/appSettings.ts
+++ sitoweb/src/appSettings.ts
@@ -1,3 +1,3 @@
 export class AppSettings {
-    public static _API='https://5000-bellottap-emptygitpod-utt6evx2150.ws-eu46.gitpod.io/';
+    public static _API='https://5000-bellottap-emptygitpod-ggvdifbie16.ws-eu46.gitpod.io/';
  }
